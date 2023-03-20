dd0912745d349f32e6883e3aea4b5500b3309758
Kajal Kumari <96781807+Kajal4367@users.noreply.github.com>
2023-03-20 14:42:49 +0530

    Create Multiple_of_5.py

---
+++ Multiple_of_5.py
@@ -0,0 +1,6 @@
+# Check whether the given integer is multiple of 5 or not.
+num = int(input("Enter an integer:"))
+if num % 5 == 0:
+  print("Given number is a multiple of 5")
+else:
+  print("Given number is not a multiple of 5")
